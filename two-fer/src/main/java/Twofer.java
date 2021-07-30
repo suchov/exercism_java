c6f1f04700134c4a7e32a2655dd3721963794b06
Artem Sychov <artem.suchov@gmail.com>
2021-07-30 13:32:26 +0300

    initiate repo with some tasks solved

---
+++ two-fer/src/main/java/Twofer.java
@@ -0,0 +1,11 @@
+public class Twofer {
+    public String twofer(String name) {
+	// Option 1 slow with String.format method
+        // return String.format("One for %s, one for me.", name == null ? "you" : name);
+	// An alternative to the ternary operator solution
+	// String you_name = java.util.Optional.ofNullable(name).orElse("you");
+	// return "One for " + you_name + ", one for me.";	
+	// short one with the concatenation
+	return "One for " + (name != null ? name : "you") + ", one for me.";	
+    }
+}
