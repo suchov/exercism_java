5e17545fe3e808516fd525a964efa09918696247
Artem Sychov <artem.suchov@gmail.com>
2021-08-03 10:20:44 +0300

    finish twofer butifully, add more tasks

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,7 +1,7 @@
 public class Twofer {
     public String twofer(String name) {
 	// Option 1 slow with String.format method
-        // return String.format("One for %s, one for me.", name == null ? "you" : name);
+	// return String.format("One for %s, one for me.", name == null ? "you" : name);
 	// An alternative to the ternary operator solution
 	// String you_name = java.util.Optional.ofNullable(name).orElse("you");
 	// return "One for " + you_name + ", one for me.";	
